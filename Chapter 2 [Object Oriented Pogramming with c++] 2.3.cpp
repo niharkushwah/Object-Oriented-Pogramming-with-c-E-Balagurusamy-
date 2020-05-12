ad53056fc9dea3ae0a9a26cd95fd72c6af199bc1
Nihar Kushwah <59207039+niharkushwah@users.noreply.github.com>
2020-05-12 18:13:39 +0530

    Add files via upload

---
+++ Chapter 2 [Object Oriented Pogramming with c++] 2.3.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+   char c;
+    cout<<"Enter THe Character";
+    cin>>c;
+    cout<<"The Character is Inserted:"<< int (c);
+    return 0;
+}
