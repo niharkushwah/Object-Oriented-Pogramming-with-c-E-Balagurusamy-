c62aca2621c8eebd65c03a0c66556ea5b2cb9c17
Nihar Kushwah <59207039+niharkushwah@users.noreply.github.com>
2020-06-22 11:26:43 +0530

    Add files via upload

---
+++ 2.4 Write a program to read the values a, b and c and display x, where x = a divide b –c.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a,b,c;
+    float x;
+    cout<<"Enter the values of a,b,c :-"<< endl;
+    cin>>a>>b>>c;
+    x=a/b-c;
+    cout<<x;
+}
