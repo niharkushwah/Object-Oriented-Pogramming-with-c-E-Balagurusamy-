3b6e4807682c6cefc7ac79e3739fcad0f0a04985
Nihar Kushwah <59207039+niharkushwah@users.noreply.github.com>
2020-06-22 11:41:30 +0530

    Add files via upload

---
+++ 2.5 Write a C++ program that will ask for a temperature in Fahrenheit and display it in Celsius .cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    float temp,fer;
+    cout<<"Enter The Temperature in Fahrenheit"<<endl;
+    cin>>temp;
+    fer = ((temp-32)/9)*5;
+    cout<<"Temperature in Celsius Is :-"<<fer;
+}
